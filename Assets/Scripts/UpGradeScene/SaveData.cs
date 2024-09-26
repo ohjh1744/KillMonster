766190f385a0f77f93372cecd708c4feba7c2d68
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-26 14:34:34 +0900

    Create SaveData.cs

---
+++ Assets/Scripts/UpGradeScene/SaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public int MissionNum;
+    [SerializeField] private float UpGradeDamage;
+    private float UpGradeMaxHp;
+
+}
