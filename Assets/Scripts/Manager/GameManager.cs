53928dab11489218946216be8717a015ce95f797
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-26 11:11:08 +0900

    Update GameManager.cs

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   
+   public GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
