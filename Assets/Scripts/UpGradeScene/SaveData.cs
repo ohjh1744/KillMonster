a865b1adc7f18f183eb7d809ce0e2647c84735de
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-26 14:50:19 +0900

    Update SaveData.cs
    업그레이드공격력, 업그레이드최대체력, 각각 업그레이드 돈, 소유 돈 변수 추가

--- Assets/Scripts/UpGradeScene/SaveData.cs
+++ Assets/Scripts/UpGradeScene/SaveData.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SaveData : MonoBehaviour
 {
     public int MissionNum;
-    [SerializeField] private float UpGradeDamage;
-    private float UpGradeMaxHp;
+    [SerializeField] private float _upGradeDamage;
+    [SerializeField] private float _upGradeMaxHp;
+    [SerializeField] private int _Gold;
+    [SerializeField] private int _upGradeDamageGold;
+    [SerializeField] private int _upGradeMaxHpGold;
 
+    public float UpGradeDamage { get { return _upGradeDamage; } set { _upGradeDamage = value; OnUpGradeDamageChanged?.Invoke(); } }
+    public float UpGradeMaxHp {  get { return _upGradeMaxHp; } set { _upGradeMaxHp = value; OnUpGradeMaxHpChagned?.Invoke(); } }
+    public int Gold { get { return _Gold; } set { _Gold = value; OnGoldChanged?.Invoke(); } }
+    public int UpGradeDamageGold { get { return _upGradeDamageGold; } set { _upGradeDamageGold = value; OnUpGradeDamageGoldChanged?.Invoke(); } }
+    public int UpGradeMaxHpGold { get { return _upGradeMaxHpGold; } set { _upGradeMaxHpGold = value; OnUpGradeMaxHpGoldChanged?.Invoke(); } }
+
+    public UnityAction OnUpGradeDamageChanged;
+    public UnityAction OnUpGradeMaxHpChagned;
+    public UnityAction OnGoldChanged;
+    public UnityAction OnUpGradeDamageGoldChanged;
+    public UnityAction OnUpGradeMaxHpGoldChanged;
 }
