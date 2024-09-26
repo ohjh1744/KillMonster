a15b2073dbeee9f1dbce98ad84d603e942dd59d6
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-26 12:27:48 +0900

    Create BulletHitImpactPull.cs
    BulletPUllManager에서 이름 변경후 싱글톤 삭제

---
+++ Assets/Scripts/BattleScene/Weapon/Gun/BulletHitImpactPull.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHitImpactPull : MonoBehaviour
+{
+    [SerializeField]private GameObject BulletImpact;
+    private List<GameObject>_pool;
+
+    public void Awake()
+    {
+        _pool = new List<GameObject>();
+    }
+    public GameObject Get()
+    {
+        Debug.Log("hi");
+        GameObject select = null;
+
+        foreach (GameObject item in _pool)
+        {
+            if (!item.activeSelf)
+            {
+                select = item;
+                select.SetActive(true);
+                break;
+            }
+        }
+
+        if (select == null)
+        {
+            select = Instantiate(BulletImpact, transform);
+            _pool.Add(select);
+        }
+
+        return select;
+    }
+}
