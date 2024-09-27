46a9b81f10b033d3676ae56a2bc4b3b25c17fa8a
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-27 11:32:10 +0900

    Update GameManager.cs
    게임 상태 생성.
    상태에 따라 게임 진행

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Timeline;
 
+public enum GameState { 준비, 시작, 끝}
 public class GameManager : MonoBehaviour
 {
-   public GameManager Instance { get; private set; }
+
+    [SerializeField] private AudioClip _bgm;
+    [SerializeField] private TimelineAsset _startTimeLine;
+    private float _finishStartTime;
+    private float _currentTime;
+    private bool _isGameStart;
+    public GameState GameState;
+
 
     private void Awake()
     {
+        GameState = GameState.준비;
+        SoundManager.Instance.PlayeBGM(_bgm);
+        _finishStartTime = (float)_startTimeLine.duration;
+    }
 
+    private void Update()
+    {
+        _currentTime += Time.deltaTime;
+        if (_currentTime > _finishStartTime)
+        {
+            StartGame();
+        }
+    }
+
+    private void StartGame()
+    {
+        if(_isGameStart == false)
+        {
+            _isGameStart = true;
+            GameState = GameState.시작;
+        }
     }
 
 }
