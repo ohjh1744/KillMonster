8a5683e51ff5e9bf40c6aee4c664f0d27b8a21ef
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-27 16:12:35 +0900

    Update MoveState.cs
    움직임 Sound 구현.

--- Assets/Scripts/BattleScene/Player/State/MoveState.cs
+++ Assets/Scripts/BattleScene/Player/State/MoveState.cs
@@ -6,16 +6,20 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MoveState : MovementState
 {
-    //private CharacterController _controller;
     private PlayerStateMachine _player;
     private PlayerData _playerData;
     private Animator _anim;
     private float _originSpeed;
     private Vector3 _moveDir;
     private Rigidbody _rigid;
-    private float canMoveTime;
-    private float currentTime;
     private bool _isRun;
+    private bool _hasStoppedRunSound;
+    private float _cantMoveTime;
+    private float _curCantMoveTime;
+    private float _walkSoundTime;
+    private float _curWalkSoundTime;
+    private float _runSoundTime;
+    private float _curRunSoundTime;
 
     public MoveState(PlayerStateMachine player)
     {
@@ -23,13 +27,18 @@ public class MoveState : MovementState
         _playerData = _player.PlayerData;
         _originSpeed = _playerData.Speed;
         _rigid = _player.Rigid;
-        canMoveTime = _playerData.StiffnessTime;
+        _cantMoveTime = _playerData.StiffnessTime;
+        _walkSoundTime = _playerData.AudioTimes[(int)Sound.°È±â];
+        _runSoundTime = _playerData.AudioTimes[(int)Sound.¶Ù±â];
     }
     public override void Enter()
     {
         Debug.Log("ÇöÀç Walk State¿¡ ÁøÀÔ!");
         _anim = _playerData.FireStates[(int)_playerData.CurFireWeapon].GetComponent<Animator>();
-        //SoundManager.Instance.PlaySFX(_playerData.AudioClips[(int)Sound.°È±â]);
+        _curWalkSoundTime = 0;
+        _curRunSoundTime = 0;
+        _isRun = false;
+        _hasStoppedRunSound = true;
     }
 
     public override void Update()
@@ -38,20 +47,19 @@ public class MoveState : MovementState
         {
             _anim = _playerData.FireStates[(int)_playerData.CurFireWeapon].GetComponent<Animator>();
         }
+
+        PlayWalkSound();
+        PlayRunSound();
         if (_playerData.IsDamage == true)
         {
-            currentTime += Time.deltaTime;
-            if(currentTime > canMoveTime)
-            {
-                _playerData.IsDamage = false;
-                currentTime = 0;
-            }
+            CantWalk();
         }
-        if(_playerData.IsDamage == false)
+        else if(_playerData.IsDamage == false)
         {
             Walk();
             Run();
         }
+
         if (_moveDir == Vector3.zero)
         {
             _player.ChangeMovementState(_player.MovementStates[(int)EMovementState.Idle]);
@@ -61,8 +69,17 @@ public class MoveState : MovementState
     public override void Exit()
     {
         Debug.Log("Walk State¿¡¼­ ³ª°¨!");
+        SoundManager.Instance.StopSFX();
     }
 
+    private void PlayWalkSound()
+    {
+        if (Time.time - _curWalkSoundTime > _walkSoundTime && _isRun == false)
+        {
+            SoundManager.Instance.PlaySFX(_playerData.AudioClips[(int)Sound.°È±â]);
+            _curWalkSoundTime = Time.time;
+        }
+    }
     private void Walk()
     {
         _moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
@@ -71,13 +88,40 @@ public class MoveState : MovementState
 
         _rigid.velocity = dir;
     }
+    private void CantWalk()
+    {
+        _curCantMoveTime += Time.deltaTime;
+        if (_curCantMoveTime > _cantMoveTime)
+        {
+            _playerData.IsDamage = false;
+            _curCantMoveTime = 0;
+        }
+    }
+
+    private void PlayRunSound()
+    {
+        if (Time.time - _curRunSoundTime > _runSoundTime && _isRun == true)
+        {
+            SoundManager.Instance.StopSFX();
+            SoundManager.Instance.PlaySFX(_playerData.AudioClips[(int)Sound.¶Ù±â]);
+            _curRunSoundTime = Time.time;
+            _hasStoppedRunSound = false;
+        }
+        if(_isRun == false && _hasStoppedRunSound == false)
+        {
+            SoundManager.Instance.StopSFX();
+            _hasStoppedRunSound = true;
+            _curWalkSoundTime = 0;
+        }
+    }
 
     private void Run()
     {
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            if(_playerData.RunGage > 0)
+            if (_playerData.RunGage > 0)
             {
+                _isRun = true;
                 _playerData.IsRun = true;
                 _playerData.RunGage -= Time.deltaTime;
                 _playerData.Speed = _originSpeed * 2;
@@ -97,6 +141,7 @@ public class MoveState : MovementState
 
     private void CantRun()
     {
+        _isRun = false;
         _playerData.Speed = _originSpeed;
         _anim.SetBool("isRun", false);
     }
