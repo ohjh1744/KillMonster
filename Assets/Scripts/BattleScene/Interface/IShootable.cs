b3fac4cc9f3a3b3c5f33ce1de344fcdf102fb264
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-27 20:09:06 +0900

    매개변수 AudioSource 추가
    매개변수 AudioSource 추가

--- Assets/Scripts/BattleScene/Interface/IShootable.cs
+++ Assets/Scripts/BattleScene/Interface/IShootable.cs
@@ -7,8 +7,8 @@ public interface IShootable
     public int Bullet { get; set; }
     public float ReCoil { get; set; }
     public bool IsReLoad { get; set; }
-    public void Shoot(Camera camera, float damage);
-    public void ReLoad();
+    public void Shoot(Camera camera, float damage, AudioSource audioSource);
+    public void ReLoad(AudioSource audioSource);
 
     public GameObject FireFlash { get; set; }
 }
