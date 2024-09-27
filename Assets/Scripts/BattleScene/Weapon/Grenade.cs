2f05929fdeb222fc84dc9ca3ee8c17bdbf5a0c69
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-27 17:28:44 +0900

    Update Grenade.cs
    수류탄 까는 소리와 터지는 소리 추가

--- Assets/Scripts/BattleScene/Weapon/Grenade.cs
+++ Assets/Scripts/BattleScene/Weapon/Grenade.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class Grenade : Explosive, IAttackTime, IThrowable
 {
-
-    [SerializeField]
-    private float _attackTime;
-    [SerializeField]
-    private float _power;
+    [SerializeField] private AudioClip _throwClip;
+    [SerializeField] private AudioClip _explodeClip;
+    [SerializeField] private float _attackTime;
+    [SerializeField] private float _power;
     private float _playerDamage;
     public float AttackTime { get { return _attackTime; } set { _attackTime = value; } }
 
     public Vector3 Target { get; set; }
     public void Throw(float damage)
     {
+        SoundManager.Instance.PlaySFX(_throwClip);
         _rigid.AddForce(_rigid.transform.forward * _power, ForceMode.Impulse);
         _playerDamage = damage;
     }
@@ -27,6 +27,7 @@ public class Grenade : Explosive, IAttackTime, IThrowable
             // explosion이 여러곳에서 발생하는 버그 fix를 위해
             if(_coroutine == null)
             {
+                SoundManager.Instance.PlaySFX(_explodeClip);
                 _coroutine = StartCoroutine(Explode(_playerDamage));
             }
         }
