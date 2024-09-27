06d676ba262f3ac53e799ad6b17bdc4256d1c2ec
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-27 16:31:15 +0900

    Update Gun.cs
    총 쏠때 오디오 추가

--- Assets/Scripts/BattleScene/Weapon/Gun/Gun.cs
+++ Assets/Scripts/BattleScene/Weapon/Gun/Gun.cs
@@ -26,8 +26,8 @@ public class GunData
 
 public class Gun : MonoBehaviour, IAttackTime, IShootable, IZoomable
 {
-   [SerializeField] private GunData _gunData;
-
+    [SerializeField] private GunData _gunData;
+    [SerializeField] private AudioClip _audioClip;
     private Coroutine _fireFlashRoutine;
     private WaitForSeconds _flashWaitForSeconds;
     private Coroutine _reLoadRoutine;
@@ -49,6 +49,7 @@ public class Gun : MonoBehaviour, IAttackTime, IShootable, IZoomable
     }
     public void Shoot(Camera camera, float playerDamage)
     {
+        SoundManager.Instance.PlaySFX(_audioClip);
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit, 100))
         {
