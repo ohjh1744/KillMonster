93e793dca269cc13543b67794f7698baf41ca861
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-27 16:12:07 +0900

    Update SoundManager.cs
    StopSFX 함수 추가

--- Assets/Scripts/Manager/SoundManager.cs
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -8,9 +8,9 @@ public class SoundManager : MonoBehaviour
 
     //SoundManager gameobject아래에 bgm, sfx object를 자식으로 둠.
     // 배경음은 거리상관없이 2d사운드식으로
-    [SerializeField] AudioSource bgm;
+    [SerializeField] private AudioSource _bgm;
     //레벨업 같은 소리는 2d사운드식으로 거리상관없이 그냥 들리도록하기위해서
-    [SerializeField] AudioSource sfx;
+    public AudioSource Sfx;
 
     private void Awake()
     {
@@ -27,45 +27,52 @@ public class SoundManager : MonoBehaviour
 
     public void PlayeBGM(AudioClip clip)
     {
-        bgm.clip = clip;
-        bgm.Play();
+        _bgm.clip = clip;
+        _bgm.Play();
     }
 
     public void StopBGM()
     {
-        if (bgm.isPlaying == false)
+        if (_bgm.isPlaying == false)
         {
             return;
         }
-        bgm.Stop();
+        _bgm.Stop();
     }
 
     public void PauseBGM()
     {
-        if (bgm.isPlaying == false)
+        if (_bgm.isPlaying == false)
         {
             return;
         }
-        bgm.Pause();
+        _bgm.Pause();
     }
 
     public void SetBGm(float volume, float pitch = 1f)
     {
-        bgm.volume = volume;
-        bgm.pitch = pitch;
+        _bgm.volume = volume;
+        _bgm.pitch = pitch;
     }
 
     public void PlaySFX(AudioClip clip)
     {
-        sfx.PlayOneShot(clip);
-        sfx.clip = clip;
-        sfx.Play();
+        Sfx.PlayOneShot(clip);
     }
 
     public void SetSFX(float volume, float pitch = 1f)
     {
-        sfx.volume = volume;
-        sfx.pitch = pitch;
+        Sfx.volume = volume;
+        Sfx.pitch = pitch;
+    }
+
+    public void StopSFX()
+    {
+        if (Sfx.isPlaying == false)
+        {
+            return;
+        }
+        Sfx.Stop();
     }
 
 }
