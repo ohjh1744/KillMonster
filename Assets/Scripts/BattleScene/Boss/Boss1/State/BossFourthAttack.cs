478b5313c20cc8096074868fa022a1e38289c7f2
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-28 20:15:13 +0900

    Update BossFourthAttack.cs
    anim -> _ani으로 변경
    _warningAnim 변수 추가 후
    경고 Image 애니메이션 재생및 false.

--- Assets/Scripts/BattleScene/Boss/Boss1/State/BossFourthAttack.cs
+++ Assets/Scripts/BattleScene/Boss/Boss1/State/BossFourthAttack.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class BossFourthAttack : BossState
 {
@@ -9,26 +10,31 @@ public class BossFourthAttack : BossState
     private BossData _bossData;
     private NavMeshAgent _navMesh;
     private BossWorldAreaAttack _bossWorldAreaAttack;
-    private Animator anim;
+    private Animator _anim;
     private AudioClip _fourthAttack;
     private AudioSource _audioSource;
+    private Animator _warningAnim;
+    private int _warningAnimTrueHash = Animator.StringToHash("WarningImageTrue");
+    private int _warningAnimFalseHash = Animator.StringToHash("WarningImageFalse");
     public BossFourthAttack(BossStateMachine boss)
     {
         this._boss = boss;
         _bossData = _boss.BossData;
-        _navMesh = _bossData.NavMesh;
+        _navMesh = _bossData.NavMesh; 
         _bossWorldAreaAttack = _boss.GetComponent<BossWorldAreaAttack>();
-        anim = _bossData.Anim;
+        _anim = _bossData.Anim;
         _fourthAttack = _bossData.AudioClips[(int)BossSound.FourthAttack];
         _audioSource = _boss.AudioSource;
+        _warningAnim = _boss.WarningImage.GetComponent<Animator>();
     }
     public override void Enter()
     {
         Debug.Log("BossFourthAttack 진입");
         _navMesh.enabled = false;
         _boss.transform.LookAt(_bossData.Player.transform);
-        anim.Play("FourthAttack", -1, 0);
-        _bossWorldAreaAttack.Attack(_bossData.BasicDamage, anim, "FourthAttack", _fourthAttack, _audioSource);
+        _anim.Play("FourthAttack", -1, 0);
+        _bossWorldAreaAttack.Attack(_bossData.BasicDamage, _anim, "FourthAttack", _fourthAttack, _audioSource);
+        _warningAnim.Play(_warningAnimTrueHash);
     }
 
     public override void Update()
@@ -46,6 +52,7 @@ public class BossFourthAttack : BossState
 
     public override void Exit()
     {
+        _warningAnim.Play(_warningAnimFalseHash);
         _bossWorldAreaAttack.IsAttack = true;
         _boss._isChange = false;
         Debug.Log("BossFourthAttack 나감");
