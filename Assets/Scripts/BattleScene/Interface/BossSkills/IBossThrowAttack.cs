4c1bc71e041d1e41bbfdbe1cbf55634e8c4ad723
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-28 23:48:35 +0900

    Create IBossThrowAttack.cs

---
+++ Assets/Scripts/BattleScene/Interface/BossSkills/IBossThrowAttack.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IBossThrowAttack
+{
+    public Transform Target { get; set; }
+    public bool IsAttack { get; set; }
+    public void Attack(float bossBasicDamage);
+}
