214f4b3e99ee8adf1925f76199ee3087d5f54d35
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-29 00:19:40 +0900

    Update BossUpsetState.cs
    Boss Upset상태 돌입시 UIImage 띄우기

--- Assets/Scripts/BattleScene/Boss/Boss1/State/BossUpsetState.cs
+++ Assets/Scripts/BattleScene/Boss/Boss1/State/BossUpsetState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class BossUpsetState : BossState
 {
@@ -10,10 +11,13 @@ public class BossUpsetState : BossState
     private BossUpset _bossUpset;
     private NavMeshAgent _navMesh;
     private Animator anim;
+    private Animator _warningAnim;
     private float _upsetTime;
 
     private float _currentTIme;
     private int _upsetHash = Animator.StringToHash("Upset");
+    private int _warningAnimTrueHash = Animator.StringToHash("WarningImageTrue");
+    private int _warningAnimFalseHash = Animator.StringToHash("WarningImageFalse");
     public BossUpsetState(BossStateMachine boss)
     {
         this._boss = boss;
@@ -22,12 +26,14 @@ public class BossUpsetState : BossState
         _navMesh = _boss.GetComponent<NavMeshAgent>();
         anim = _boss.GetComponent<Animator>();
         _upsetTime = _bossData.UpsetTime;
+        _warningAnim = _boss.UpsetWarningImage.GetComponent<Animator>();
     }
     public override void Enter()
     {
         Debug.Log("BossUpsetState 진입");
         _navMesh.enabled = false;
         anim.Play(_upsetHash);
+        _warningAnim.Play(_warningAnimTrueHash);
         _bossUpset.TurnUpset(_bossData);
     }
 
@@ -47,6 +53,7 @@ public class BossUpsetState : BossState
 
     public override void Exit()
     {
+        _warningAnim.Play(_warningAnimFalseHash);
         _boss._isChange = false;
         _currentTIme = 0;
         Debug.Log("BossUpsetState 나감");
