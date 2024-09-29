1657657e21c119fde4a63f0133e35b95edfe0636
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-29 22:25:11 +0900

    Create BossDead.cs
    BossDead 스킬 생성

---
+++ Assets/Scripts/BattleScene/Boss/Boss1/Skills/BossDead.cs
@@ -0,0 +1,23 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossDead : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _deadClip;
+    [SerializeField] private CinemachineVirtualCamera _bossCamera;
+    [SerializeField] private int priorityCamera;
+
+    private float _deadTime;
+    private float _currentTime;
+
+    public void Dead()
+    {
+        _bossCamera.Priority = priorityCamera;
+        _audioSource.Stop();
+        _audioSource.clip = _deadClip;
+        _audioSource.Play();
+    }
+}
