0a6e247422199628826baf464d16fdce0be796f2
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-29 22:25:01 +0900

    Update BossData.cs
    Money 이름 GOld로 수정

--- Assets/Scripts/BattleScene/Boss/Boss1/BossData.cs
+++ Assets/Scripts/BattleScene/Boss/Boss1/BossData.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
-public enum BossSound {Walk, Upset, SecondAttack, ThirdAttack, FourthAttack}
 public class BossData : MonoBehaviour
 {
     [SerializeField] private float _hp;
     [SerializeField]private float _maxhp;
-    public int Money;
+    public int Gold;
     public float Speed;
     public int BasicDamage;
     [Header("Upset")]
     public float UpsetTime;
     [HideInInspector] public bool IsUpset;
+    [Header("Dead")]
+    public float DeadTime;
 
     public float Hp { get { return _hp; } set { _hp = value; OnHpChanged?.Invoke(); } }
     public float MaxHp { get { return _maxhp; } set { _maxhp = value;} }
