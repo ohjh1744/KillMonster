bc6516b50d2486152df2d12d799fc489d478ba03
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-30 14:54:43 +0900

    Update PlayerStateMachine.cs
    게임이 Pause일때는 Player 잠시 멈춤

--- Assets/Scripts/Scenes/BattleScene/Player/State/PlayerStateMachine.cs
+++ Assets/Scripts/Scenes/BattleScene/Player/State/PlayerStateMachine.cs
@@ -53,7 +53,7 @@ public class PlayerStateMachine : MonoBehaviour, IDamagable
     private void Update()
     {
 
-        if(_gameManager.GameState == GameState.시작)
+        if(_gameManager.GameState == GameState.시작 && _gameManager.IsGamePause == false)
         {
             Dead();
             UpRunGage();
