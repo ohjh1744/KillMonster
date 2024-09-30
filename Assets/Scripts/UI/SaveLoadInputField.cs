35cb3c252a142d83d0b6a59e4592dc6d2efe39fc
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-30 12:58:55 +0900

    Create SaveLoadInputField.cs
    mainScene Save Load창 켰다껏다기능

---
+++ Assets/Scripts/UI/SaveLoadInputField.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveLoadInputField : MonoBehaviour
+{
+    [SerializeField] private InputField _saveInputField;
+    [SerializeField] private InputField _loadInputField;
+    void Start()
+    {
+        _saveInputField.onSubmit.AddListener(DataManager.Instance.Save);
+        _loadInputField.onSubmit.AddListener(DataManager.Instance.Load);
+    }
+
+
+    public void ShowInputField(GameObject inputUI)
+    {
+        if(inputUI.activeSelf == true)
+        {
+            inputUI.SetActive(false);
+        }
+        else
+        {
+            inputUI.SetActive(true);
+        }
+    }
+
+
+}
