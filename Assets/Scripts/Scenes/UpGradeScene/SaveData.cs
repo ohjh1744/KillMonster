03d11adb203bcdeac919cd53e37178fa405af8df
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-09-30 12:59:55 +0900

    Update SaveData.cs
    enum 앞에 모두 E 붙임.
    GameData 생성 후 GameData 안에 모든 저장 변수 옮김.

--- Assets/Scripts/Scenes/UpGradeScene/SaveData.cs
+++ Assets/Scripts/Scenes/UpGradeScene/SaveData.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public enum Mission {데몬킹, 좀비킹, 언데드킹 }
-public class SaveData : MonoBehaviour
+public enum EMission {데몬킹, 좀비킹, 언데드킹 }
+public enum EStatType {데미지, 최대Hp }
+
+[System.Serializable]
+public class GameData
 {
     public bool[] ClearMissions;
-    [SerializeField] private Stat DamageState;
-    [SerializeField] private Stat MaxHpState;
-    [SerializeField] private int _gold;
+    public Stat[] Stat;
+    public BaseStat[] BaseStat;
+    public int Gold;
+}
 
-    public float CurrentDamage { get { return DamageState.CurrentStat; } set { DamageState.CurrentStat = value;} }
-    public float CurrentMaxHp { get { return MaxHpState.CurrentStat; } set { MaxHpState.CurrentStat = value; } }
-    public float Damage { get { return DamageState.UpGradeStat; } set { DamageState.UpGradeStat = value; OnUpGradeDamageChanged?.Invoke(); } }
-    public float MaxHp {  get { return MaxHpState.UpGradeStat; } set { MaxHpState.UpGradeStat = value; OnUpGradeMaxHpChagned?.Invoke(); } }
-    public int Gold { get { return _gold; } set { _gold = value; OnGoldChanged?.Invoke(); } }
-    public int UpGradeDamageGold { get { return DamageState.UpGradeStatGold; } set { DamageState.UpGradeStatGold = value; OnUpGradeDamageGoldChanged?.Invoke(); } }
-    public int UpGradeMaxHpGold { get { return MaxHpState.UpGradeStatGold; } set { MaxHpState.UpGradeStatGold = value; OnUpGradeMaxHpGoldChanged?.Invoke(); } }
-    public int UpGradeDamageLevel { get { return DamageState.Level; } set { DamageState.Level = value; OnUpGradeDmaageLevel?.Invoke(); } }
-    public int UpGradeMaxHpLevel { get { return MaxHpState.Level; } set { MaxHpState.Level = value; OnUpGradeMaxHpLevel?.Invoke(); } }
-    public int DamageMaxLevel {  get { return DamageState.MaxLevel; } set { DamageState.MaxLevel = value; } }
-    public int MaxHpMaxLevel { get { return MaxHpState.MaxLevel; } set { MaxHpState.MaxLevel = value; } }
+[CreateAssetMenu( menuName = "ScriptableObjects/SaveData")]
+public class SaveData : ScriptableObject
+{
+    public  GameData GameData;
+
+    public float CurrentDamage { get { return GameData.Stat[(int)EStatType.데미지].CurrentStat; } set { GameData.Stat[(int)EStatType.데미지].CurrentStat = value;} }
+    public float CurrentMaxHp { get { return GameData.Stat[(int)EStatType.최대Hp].CurrentStat; } set { GameData.Stat[(int)EStatType.최대Hp].CurrentStat = value; } }
+    public float Damage { get { return GameData.Stat[(int)EStatType.데미지].UpGradeStat; } set { GameData.Stat[(int)EStatType.데미지].UpGradeStat = value; OnUpGradeDamageChanged?.Invoke(); } }
+    public float MaxHp {  get { return GameData.Stat[(int)EStatType.최대Hp].UpGradeStat; } set { GameData.Stat[(int)EStatType.최대Hp].UpGradeStat = value; OnUpGradeMaxHpChagned?.Invoke(); } }
+    public int Gold { get { return GameData.Gold; } set { GameData.Gold = value; OnGoldChanged?.Invoke(); } }
+    public int UpGradeDamageGold { get { return GameData.Stat[(int)EStatType.데미지].UpGradeStatGold; } set { GameData.Stat[(int)EStatType.데미지].UpGradeStatGold = value; OnUpGradeDamageGoldChanged?.Invoke(); } }
+    public int UpGradeMaxHpGold { get { return GameData.Stat[(int)EStatType.최대Hp].UpGradeStatGold; } set { GameData.Stat[(int)EStatType.최대Hp].UpGradeStatGold = value; OnUpGradeMaxHpGoldChanged?.Invoke(); } }
+    public int UpGradeDamageLevel { get { return GameData.Stat[(int)EStatType.데미지].Level; } set { GameData.Stat[(int)EStatType.데미지].Level = value; OnUpGradeDmaageLevel?.Invoke(); } }
+    public int UpGradeMaxHpLevel { get { return GameData.Stat[(int)EStatType.최대Hp].Level; } set { GameData.Stat[(int)EStatType.최대Hp].Level = value; OnUpGradeMaxHpLevel?.Invoke(); } }
+    public int DamageMaxLevel {  get { return GameData.Stat[(int)EStatType.데미지].MaxLevel; } set { GameData.Stat[(int)EStatType.데미지].MaxLevel = value; } }
+    public int MaxHpMaxLevel { get { return GameData.Stat[(int)EStatType.최대Hp].MaxLevel; } set { GameData.Stat[(int)EStatType.최대Hp].MaxLevel = value; } }
 
     public UnityAction OnUpGradeDamageChanged;
     public UnityAction OnUpGradeMaxHpChagned;
