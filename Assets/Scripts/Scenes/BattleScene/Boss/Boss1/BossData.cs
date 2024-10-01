3220d7252c6c20b4de1eff7473db0b5066d131ac
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-02 00:04:28 +0900

    Update BossData.cs
    변수정리

--- Assets/Scripts/Scenes/BattleScene/Boss/Boss1/BossData.cs
+++ Assets/Scripts/Scenes/BattleScene/Boss/Boss1/BossData.cs
@@ -6,18 +6,35 @@ using UnityEngine.Events;
 
 public class BossData : MonoBehaviour
 {
+    [Header("Basic Stat")]
     [SerializeField] private float _hp;
-    [SerializeField]private float _maxhp;
-    public int Gold;
-    public float Speed;
-    public int BasicDamage;
-    [Header("Upset")]
-    public float UpsetTime;
-    [HideInInspector] public bool IsUpset;
-    [Header("Dead")]
-    public float DeadTime;
-
     public float Hp { get { return _hp; } set { _hp = value; OnHpChanged?.Invoke(); } }
-    public float MaxHp { get { return _maxhp; } set { _maxhp = value;} }
+
+    [SerializeField] private float _maxhp;
+    public float MaxHp { get { return _maxhp; } private set { } }
+
+    [SerializeField] private int _gold;
+    public int Gold { get { return _gold; } private set { } }
+
+    [SerializeField] private float _speed;
+    public float Speed { get { return _speed; } set { _speed = value; } }
+
+    [SerializeField] private float _damage;
+    public float Damage { get { return _damage; } set { _damage = value; } }
+
+    [Header("Upset")]
+
+    [SerializeField] private float _upsetTime;
+    public float UpsetTime { get { return _upsetTime; } private set { } }
+
+    public bool IsUpset { get; set; }
+
+    [Header("Dead")]
+
+    [SerializeField] private float _deadTime;
+    public float DeadTime { get { return _deadTime; } private set { } }
+
+
+
     public UnityAction OnHpChanged;
 }
