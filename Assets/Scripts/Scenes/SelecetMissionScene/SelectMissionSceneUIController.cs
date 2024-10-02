19e21a027dc9cc416317f466381a5a5fe9feca18
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-02 16:36:28 +0900

    SelectMissionSceneUIController로 이름 변경

--- Assets/Scripts/Scenes/SelecetMissionScene/SelectMissionSceneUIController.cs
+++ Assets/Scripts/Scenes/SelecetMissionScene/SelectMissionSceneUIController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SelectMissionSceneUI : MonoBehaviour
+public class SelectMissionSceneUIController : MonoBehaviour
 {
     [SerializeField] private GameObject[] missions;
 
