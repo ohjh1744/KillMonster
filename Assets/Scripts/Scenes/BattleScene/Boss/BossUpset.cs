c2bbda896b06857ece9cc1ef7cefe5ab441dcbcc
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-10-03 18:43:36 +0900

    Boss 폴더로이동
    공통 클래스이므로 폴더이동

---
+++ Assets/Scripts/Scenes/BattleScene/Boss/BossUpset.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossUpset : MonoBehaviour
+{
+
+    [SerializeField] AudioSource _audioSource;
+
+    [SerializeField] AudioClip _upsetClip;
+
+    private float _curMoveSoundTime;
+
+    public void TurnUpset(BossData bossData)
+    {
+        _audioSource.clip = _upsetClip;
+        _audioSource.Play();
+        bossData.IsUpset = true;
+        bossData.Damage *= 2;
+        bossData.Speed *= 2;
+    }
+}
